50ed43090fa7d1bc458488864cbd90c83e417ecd
Verona0609 <dikaya.nika15@gmail.com>
2024-09-16 20:07:55 +0200

    HOMEWORK-08

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,13 +1,12 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import { instanse } from "../auth/operations";
 
 export const fetchContacts = createAsyncThunk(
   "contacts/fetchAll",
   async (_, thunkAPI) => {
     try {
-      const { data } = await axios.get(
-        "https://66d446ef5b34bcb9ab3e23ea.mockapi.io/contacts"
-      );
+      const { data } = await instanse.get("/contacts");
       return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -19,10 +18,7 @@ export const addContact = createAsyncThunk(
   "contact/addContact",
   async (newContact, thunkAPI) => {
     try {
-      const response = await axios.post(
-        "https://66d446ef5b34bcb9ab3e23ea.mockapi.io/contacts",
-        newContact
-      );
+      const response = await instanse.post("/contacts", newContact);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -34,9 +30,7 @@ export const deleteContact = createAsyncThunk(
   "contact/delete/contact",
   async (contactId, thunkAPI) => {
     try {
-      await axios.delete(
-        `https://66d446ef5b34bcb9ab3e23ea.mockapi.io/contacts/${contactId}`
-      );
+      await instanse.delete(`/contacts/${contactId}`);
       return contactId;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
